0d907fe0b98f6c754ac4298867a2f04b6c991215
edouardlicn <2361094570@qq.com>
2018-08-26 04:27:55 +0800

    Update rpserver.sql

--- rpserver.sql
+++ rpserver.sql
@@ -19,7 +19,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `rage_omp_test`
+-- Database: `rpserver`
 --
 
 -- --------------------------------------------------------
